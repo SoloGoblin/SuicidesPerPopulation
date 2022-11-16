e7eb9d8280141b5d384e2b227233f971a81bfc33
Solomon K <95310414+SoloGoblin@users.noreply.github.com>
2022-11-16 09:51:25 -0500

    a few hours of generateJson Work
    Doesn't work yet

--- data/generateJSON.js
+++ data/generateJSON.js
@@ -6,18 +6,77 @@ let suicides_csv = fs.readFileSync('suicideCSV.csv', 'utf8');
 
 let suicides = suicides_csv.split("\n");
 
-suicides.forEach(function(suicide) {
-  let suicideInfo = suicide.split(',');
+let countryName;
 
-  let suicideCountry = suicideInfo[0];
+let count = 0;
 
-  if(suicideCountry!="LOCATION"){
-    let suicideStats = {};
-    suicideStats['year'] = suicideInfo[5];
-    suicideStats['value'] = suicideInfo[6];
+suicides.forEach(function(suicideLine) {
 
-    countries[suicideCountry]=suicideStats;
+  let suicideData = suicideLine.split(',');
+
+  countryName = suicideData[0];
+
+  let suicideStats = {};
+
+  let years = {};
+
+  for(let i=count; i<suicides.length; i++) {
+    if(countryName!=suicideData[0]) {
+      break;
+    }
+    if(countryName!="LOCATION") {
+      suicideStats[suicideData[5]] = suicideData[6];
+    }
   }
 });
+/*
+let prevCountry;
+let countryName = suicideInfo[0];
+while(countryName == oldCountry) {
+  suicides.forEach(function(suicide) {
+
+    let suicideInfo = suicide.split(',');
+
+    let countryName = suicideInfo[0];
+
+    let suicideStats = {};
+
+    if(countryName!="LOCATION") {
+        suicideStats[suicideInfo[5]] = suicideInfo[6];
+    }
+      //console.log("year = " + suicideInfo[5] + ", amount = " + suicideInfo[6]);
+      // Object.assign(countries[countryName], {suicideInfo[5]: suicideInfo[6]});
+    }
+    prevCountry = countryName;
+  });
+}
+*/
+/*
+suicides.forEach(function(suicide) {
+
+  let suicideInfo = suicide.split(',');
+
+  let countryName = suicideInfo[0];
+
+  let suicideStats = {};
+
+  if(countryName!="LOCATION"){
+    if(countryName = oldCountry) {
+      suicideStats[suicideInfo[5]] = suicideInfo[6];
+
+    } else {
+      countries[countryName] = {};
+    }
+    //console.log("year = " + suicideInfo[5] + ", amount = " + suicideInfo[6]);
+    // Object.assign(countries[countryName], {suicideInfo[5]: suicideInfo[6]});
+  }
+  oldCountry = suicideCounty;
+});
+*/
 
 fs.writeFileSync('suicides.json', JSON.stringify(countries), 'utf8');
+/*
+console.log(countries)
+Object.assign(countries, {"suicide stuff" : 12});
+console.log(countries);
+*/
