bbb82f10dfc381aa51f071bddc068b118085dd55
nicoleseg <90466563+nicoleseg@users.noreply.github.com>
2022-12-04 23:29:47 -0500

    update

--- data/generateJSON.js
+++ data/generateJSON.js
@@ -17,7 +17,6 @@ suicides.forEach(function(suicideLine) {
     let suicideStats = {};
     for(let i=0; i< suicides.length; i++) {
       let suicideData2 = suicides[i].split(',');
-      //console.log(suicideData2);
       if(suicideData2[0]==countryName) {
         suicideStats[suicideData2[5]] = suicideData2[6];
       }
@@ -26,64 +25,4 @@ suicides.forEach(function(suicideLine) {
   }
 
 });
-/*
-for(let i=count; i<suicides.length; i++) {
-  if(countryName!=suicideData[0]) {
-    break;
-  }
-  if(countryName!="LOCATION") {
-    suicideStats[suicideData[5]] = suicideData[6];
-  }
-}
-*/
-/*
-let prevCountry;
-let countryName = suicideInfo[0];
-while(countryName == oldCountry) {
-  suicides.forEach(function(suicide) {
-
-    let suicideInfo = suicide.split(',');
-
-    let countryName = suicideInfo[0];
-
-    let suicideStats = {};
-
-    if(countryName!="LOCATION") {
-        suicideStats[suicideInfo[5]] = suicideInfo[6];
-    }
-      //console.log("year = " + suicideInfo[5] + ", amount = " + suicideInfo[6]);
-      // Object.assign(countries[countryName], {suicideInfo[5]: suicideInfo[6]});
-    }
-    prevCountry = countryName;
-  });
-}
-*/
-/*
-suicides.forEach(function(suicide) {
-
-  let suicideInfo = suicide.split(',');
-
-  let countryName = suicideInfo[0];
-
-  let suicideStats = {};
-
-  if(countryName!="LOCATION"){
-    if(countryName = oldCountry) {
-      suicideStats[suicideInfo[5]] = suicideInfo[6];
-
-    } else {
-      countries[countryName] = {};
-    }
-    //console.log("year = " + suicideInfo[5] + ", amount = " + suicideInfo[6]);
-    // Object.assign(countries[countryName], {suicideInfo[5]: suicideInfo[6]});
-  }
-  oldCountry = suicideCounty;
-});
-*/
-
 fs.writeFileSync('suicides.json', JSON.stringify(countries), 'utf8');
-/*
-console.log(countries)
-Object.assign(countries, {"suicide stuff" : 12});
-console.log(countries);
-*/
