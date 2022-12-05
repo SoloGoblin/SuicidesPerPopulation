16d0b1f73f374528c302e91839e1fe7433466260
Solomon K <95310414+SoloGoblin@users.noreply.github.com>
2022-12-05 02:37:16 -0500

    Here is build

---
+++ build/preBuild.js
@@ -0,0 +1,15 @@
+/* const fs = require("fs");
+const path = require("path");
+const mv = require("mv");
+
+const oldLocation = path.join(__dirname, "src", "styles.css");
+const newLocation = path.join(__dirname, "public", "styles.css");
+
+fs.rename(oldLocation, newLocation, function (err) {
+    if (err) {
+        throw err
+    } else {
+        console.log("Successfully moved the file!");
+    }
+});
+*/
