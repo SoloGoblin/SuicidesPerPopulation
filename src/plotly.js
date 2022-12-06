97e9f4f44752bf5442d4b8ccb23698bdee301fca
Solomon K <95310414+SoloGoblin@users.noreply.github.com>
2022-12-06 10:27:30 -0500

    Map ish

--- src/plotly.js
+++
@@ -1,5 +0,0 @@
-var data = [{type: 'densitymapbox', lon: [10, 20, 30], lat: [15, 25, 35], z: [1, 3, 2]}];
-
-var layout = {width: 600, height: 400, mapbox: {style: 'stamen-terrain'}};
-
-Plotly.newPlot('plotly', data, layout);
